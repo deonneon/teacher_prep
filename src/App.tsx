9ef48626af5ef10a5ba9b3a0c63556c56d9a2bfe
deonneon <dtnifti@gmail.com>
2023-12-29 18:29:03 -0600

    Add temp mobile fix

--- src/App.tsx
+++ src/App.tsx
@@ -141,7 +141,10 @@ const App: React.FC = () => {
               <div>
                 {url && (
                   <>
-                    <iframe src={url} className="pdf-iframe" title="Worksheet PDF"></iframe>
+                    <div>
+                      <iframe src={url} className="pdf-iframe large-screen" title="Large Screen PDF" />
+                      <iframe src={`${url}#zoom=50`} className="pdf-iframe small-screen" title="Small Screen PDF" />
+                    </div>
                     <a href={url} download="math-worksheet.pdf" className="download-button">
                       Download PDF
                     </a>
