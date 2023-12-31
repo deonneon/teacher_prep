b65103d71fe85235f08f0b0165510222cb94dc8c
deonneon <dtnifti@gmail.com>
2023-12-30 23:34:34 -0600

    attempt

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+// Filename - pages/index.js
+ 
+import Image from "next/image";
+import logo from "../src/logo.svg";
+ 
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Image
+                    src={logo}
+                    className="App-logo"
+                    width={200}
+                    height={200}
+                    alt="logo"
+                />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to
+                    reload.
+                </p>
+ 
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+ 
+export default App;
